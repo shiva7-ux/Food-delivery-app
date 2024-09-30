77899cafef039671f2ac616cb8211eb9fea24cda
shiva7-ux <osashivakumar19@gmail.com>
2024-09-30 18:45:55 +0530

    second episode

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -18,7 +18,7 @@ const RestaurantCard=(props) =>{
        <div className="res-card  m-4 p-4  w-[250px]  rounded-2xl bg-gray-100  hover:bg-gray-200" >
           <img  className="res-logo  rounded-lg" res alt="res-logo" 
                 src={CDN_URL + resData?.info?.cloudinaryImageId}/>
-            <h3 className="font-bold pt-10 text-lg">{name}</h3>
+            <h3 className="font-bold py-4 text-lg">{name}</h3>
             <h4>{cuisines.join(",")}</h4>
             <h4>{avgRating} stars</h4>
             <h4>{costForTwo}</h4>
